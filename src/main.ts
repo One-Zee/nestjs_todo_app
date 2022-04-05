600ac3004bfeff9e5a511b3ae7c9e7cfd81bf82d
One-Zee <harunadindane15@gmail.com>
2022-04-05 11:38:46 +0200

    added swagger to main.ts

--- src/main.ts
+++ src/main.ts
@@ -2,14 +2,27 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import helmet from 'helmet';
 import { ValidationPipe } from '@nestjs/common'
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+  .setTitle('Todo API example')
+  .setDescription('The Todo API description')
+  .setVersion('1.0.0')
+  .addTag('todo')
+  .build();
+
   app.use(helmet());
   app.enableCors();
   app.useGlobalPipes(new ValidationPipe({
     whitelist: true //removes extra data from body
   }))
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(5000);
 }
 bootstrap();
